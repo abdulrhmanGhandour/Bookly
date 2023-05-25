d5bf2c3a3f8c96499f4230ac3969865e9114cf1a
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-05-25 20:17:38 +0300

    navigate to home view

--- lib/constens.dart
+++ lib/constens.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color KPrimaryColor = Color(0xff100B20);
 const KFontBlackOpsOne = 'BlackOpsOne';
 const AppName ='BOOKLY';
+const KTransitionDuration = Duration(milliseconds: 200);
--- lib/features/splash/presanteion/view/widget/splas_view_body.dart
+++ lib/features/splash/presanteion/view/widget/splas_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:bookly_app/features/home/presanteion/view/home_view.dart';
 import 'package:bookly_app/features/splash/presanteion/view/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import '../../../../../constens.dart';
 
@@ -18,16 +21,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-            .animate(animationController);
+    initSlidingAnimation();
 
-    animationController.forward();
+    navigateToHome();
   }
+
+
 
   @override
   void dispose() {
@@ -55,5 +54,24 @@ class _SplashViewBodyState extends State<SplashViewBody>
         ),
       ),
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+    void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: KTransitionDuration);
+    });
   }
 }
