e4e945e17baafd67a4bfb3f84815077760957ac5
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-06-15 02:18:48 +0300

    add custom scroll view

--- lib/features/home/presanteion/view/widget/book_details_view_body.dart
+++ lib/features/home/presanteion/view/widget/book_details_view_body.dart
@@ -12,71 +12,77 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const CustomAppBarBookDetails(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * .33,
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 35,
-          ),
-          Text(
-            'The Jungle Book',
-            style: Styles.textStyle30
-                .copyWith(fontFamily: kFontLora, fontWeight: FontWeight.w600),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Text(
-            'Rudyard Kipling',
-            style: Styles.textStyle18.copyWith(
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
-              color: Colors.white.withOpacity(0.75),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          const BookAction(),
-          const SizedBox(
-            height: 35,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also like',
-                style: Styles.textStyle16.copyWith(
-                  fontWeight: FontWeight.w600,
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              const CustomAppBarBookDetails(),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .33,
+                child: const CustomBookImage(),
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              Text(
+                'The Jungle Book',
+                style: Styles.textStyle30.copyWith(
+                    fontFamily: kFontLora, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white.withOpacity(0.75),
                 ),
               ),
-            ),
+              const SizedBox(
+                height: 16,
+              ),
+              const BookRating(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              const BookAction(),
+              const Expanded(
+                child: SizedBox(
+                  height:50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle16.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .15,
+                child: const SimilarBooksListView(),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 18,
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * .15,
-            child: const SimilarBooksListView(),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
-
