77f05de36f48e33a19fad10a4685733dd38aa45e
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-06-22 16:30:37 +0300

    cerate failure file

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/features/home/data/model/book%20model/book_model.dart';
+import 'package:dartz/dartz.dart';
+import '../../../../core/errors/failuer.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
