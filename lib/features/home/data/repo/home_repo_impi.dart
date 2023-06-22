8a408c8abd7dc6b58bdc3f5596df7dd5e479be23
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-06-22 19:27:14 +0300

    implements fetch newset books

--- lib/features/home/data/repo/home_repo_impi.dart
+++ lib/features/home/data/repo/home_repo_impi.dart
@@ -1,19 +1,32 @@
 import 'package:bookly_app/core/errors/failuer.dart';
+import 'package:bookly_app/core/utls/api_service.dart';
 import 'package:bookly_app/features/home/data/model/book%20model/book_model.dart';
 import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpi implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpi(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailuer());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-  
     throw UnimplementedError();
   }
-  
 }
