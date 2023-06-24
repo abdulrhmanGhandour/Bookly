421f400ee025a9616ea3b12ad6086288456b43e1
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-06-24 20:54:27 +0300

    create server failuer from dio error

--- lib/core/errors/failuer.dart
+++ lib/core/errors/failuer.dart
@@ -1,3 +1,53 @@
-abstract class Failure{}
+import 'package:dio/dio.dart';
 
-class ServerFailuer extends Failure{}
+abstract class Failure {
+  final errMesage;
+
+  Failure(this.errMesage);
+}
+
+class ServerFailuer extends Failure {
+  ServerFailuer(super.errMesage);
+
+  factory ServerFailuer.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailuer('Connection time out with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailuer('Send time out with ApiServer');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailuer('Receive time out with ApiServer');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailuer('Bad Certificate with ApiServer');
+
+      case DioExceptionType.badResponse:
+        return ServerFailuer('Requset with ApiServer wsa bad response');
+
+      case DioExceptionType.cancel:
+        return ServerFailuer('Requset with ApiServer was cancel');
+
+      case DioExceptionType.connectionError:
+        return ServerFailuer('Connection was error with ApiServer');
+
+      case DioExceptionType.unknown:
+        return ServerFailuer('Opps, Unknown error');
+      default:
+        return ServerFailuer('Opps,Error');
+    }
+  }
+
+  factory ServerFailuer.fromResponse(int stutsCode, dynamic response) {
+    if (stutsCode == 400 || stutsCode == 401 || stutsCode == 403) {
+      return ServerFailuer(response["error"]["message"]);
+    } else if (stutsCode == 404) {
+      return ServerFailuer('not found ,, try later');
+    } else if (stutsCode == 500) {
+      return ServerFailuer('Server error , try later');
+    } else {
+      return ServerFailuer('Opps,Error');
+    }
+  }
+}
--- lib/features/home/data/repo/home_repo_impi.dart
+++ lib/features/home/data/repo/home_repo_impi.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/utls/api_service.dart';
 import 'package:bookly_app/features/home/data/model/book%20model/book_model.dart';
 import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpi implements HomeRepo {
   final ApiService apiService;
@@ -21,7 +22,12 @@ class HomeRepoImpi implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailuer());
+      if (e is DioException) {
+        return left(ServerFailuer.fromDioException(e));
+      }
+      return left(
+        ServerFailuer(e.toString()),
+      );
     }
   }
 
