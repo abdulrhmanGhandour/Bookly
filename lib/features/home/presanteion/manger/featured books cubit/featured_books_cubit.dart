4faaeee94fc42e38e5818bd15f0b26111b418d30
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2023-06-25 04:13:52 +0300

    create featured books cubiit method

--- lib/features/home/presanteion/manger/featured books cubit/featured_books_cubit.dart
+++ lib/features/home/presanteion/manger/featured books cubit/featured_books_cubit.dart
@@ -1,9 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
 import '../../../data/model/book model/book_model.dart';
 import '../../../data/repo/home_repo.dart';
-
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
